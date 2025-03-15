aa5582d6570be620f4777894fe64b5b125a868e8
Daksh Shah <118352018+Daxvshah11@users.noreply.github.com>
2025-03-15 16:30:34 +0530

    Update version. Add Sharing transaction
    Added:
    - request queue
    - max tries 
    - requests done
    - threading for the above
    - sharing transaction implemented
    - error handling 
    - some other things maybe

--- client/client.py
+++ client/client.py
@@ -2,6 +2,8 @@ import grpc
 import sys
 from concurrent import futures
 import time
+import socket
+import threading
 
 # changing path for importing rest of the files
 sys.path.append("../protofiles")
@@ -9,8 +11,140 @@ import services_pb2_grpc as services1
 import services_pb2 as services2
 
 
+# GLOBALS
+MY_PORT = None
+REQUEST_ID = 0
+REQUEST_QUEUE = []
+REQUEST_DONE = []
+MAX_RETRIES = 5
+RUN_QUEUE_RETRY = threading.Event()
+
+
+# fucntion for requests queued thread
+def queuedReqRetry(gatewayChannel, gatewayStub):
+    global REQUEST_QUEUE, REQUEST_DONE
+
+    # getting into loop
+    while not RUN_QUEUE_RETRY.is_set():
+        # iterating over the Queue
+        if len(REQUEST_QUEUE) > 0:
+            # checking the connection or its presence (online)
+            try:
+                grpc.channel_ready_future(gatewayChannel).result(timeout=2)
+            except grpc.FutureTimeoutError:
+                continue
+
+            # getting first element & removing it
+            req = REQUEST_QUEUE[0][0]
+            triesDone = REQUEST_QUEUE[0][1]
+            REQUEST_QUEUE = REQUEST_QUEUE[1:]
+
+            if req.__class__ == services2.RegReq:
+                try:
+                    response = gatewayStub.signUp(req)
+
+                    # checking if error occurred then queue it up at the back
+                    if response.successAck == 0:
+                        # checking if ran out of retries
+                        if (triesDone + 1) == MAX_RETRIES:
+                            REQUEST_DONE.append([req.reqID, "Request Aborted!"])
+                        else:
+                            REQUEST_QUEUE.append([req, triesDone + 1])
+                    elif response.successAck == 1:
+                        REQUEST_DONE.append([req.reqID, response.message])
+
+                except grpc.RpcError as e:
+                    if e.code() == grpc.StatusCode.UNAVAILABLE:
+                        print("Gateway server unavailable!")
+                    else:
+                        print(f"gRPC error: {e}")
+                    continue
+
+            elif req.__class__ == services2.TransactReq:
+                try:
+                    response = gatewayStub.transact(req)
+
+                    # checking if error occurred then queue it up at the back
+                    if response.successAck == 0:
+                        # checking if ran out of retries
+                        if (triesDone + 1) == MAX_RETRIES:
+                            REQUEST_DONE.append([req.reqID, "Request Aborted!"])
+                        else:
+                            REQUEST_QUEUE.append([req, triesDone + 1])
+                    elif response.successAck == 1:
+                        REQUEST_DONE.append(
+                            [req.reqID, response.message, response.balanceLeft]
+                        )
+
+                except grpc.RpcError as e:
+                    if e.code() == grpc.StatusCode.UNAVAILABLE:
+                        print("Gateway server unavailable!")
+                    else:
+                        print(f"gRPC error: {e}")
+                    continue
+
+            elif req.__class__ == services2.FailoverReq:
+                try:
+                    response = gatewayStub.failover(req)
+
+                    # checking if failed then adding to the queue
+                    if response.successAck == 0:
+                        # checking if ran out of retries
+                        if (triesDone + 1) == MAX_RETRIES:
+                            REQUEST_DONE.append([req.reqID, "Request Aborted!"])
+                        else:
+                            REQUEST_QUEUE.append([req, triesDone + 1])
+                    elif response.ack == 1:
+                        REQUEST_DONE.append([req.reqID, response.message])
+
+                except grpc.RpcError as e:
+                    if e.code() == grpc.StatusCode.UNAVAILABLE:
+                        print("Gateway server unavailable!")
+                    else:
+                        print(f"gRPC error: {e}")
+                    continue
+
+            elif req.__class__ == services2.ShareReq:
+                try:
+                    response = gatewayStub.share(req)
+
+                    # checking if error occurred then queue it up at the back
+                    if response.successAck == 0:
+                        # checking if ran out of retries
+                        if (triesDone + 1) == MAX_RETRIES:
+                            REQUEST_DONE.append([req.reqID, "Request Aborted!"])
+                        else:
+                            REQUEST_QUEUE.append([req, triesDone + 1])
+                    elif response.successAck == 1:
+                        REQUEST_DONE.append(
+                            [req.reqID, response.message, response.balanceLeft]
+                        )
+
+                except grpc.RpcError as e:
+                    if e.code() == grpc.StatusCode.UNAVAILABLE:
+                        print("Gateway server unavailable!")
+                    else:
+                        print(f"gRPC error: {e}")
+                    continue
+
+            else:
+                pass
+
+        time.sleep(1)
+
+    return
+
+
 # the Client function
 def client():
+    global MY_PORT, REQUEST_ID, REQUEST_QUEUE
+
+    # asking for the Client ID (PORT ID)
+    MY_PORT = int(input("Enter the Client (PORT) ID : "))
+
+    # printing port
+    print(f"Client running with {MY_PORT} ID ....")
+
     # loading SSL certificates
     privateKey = None
     certificate = None
@@ -35,15 +169,37 @@ def client():
     )
     gatewayStub = services1.ClientToGatewayStub(gatewayChannel)
 
+    # creating thread for queued requests
+    queueRetryThread = threading.Thread(
+        target=queuedReqRetry, args=(gatewayChannel, gatewayStub)
+    )
+    queueRetryThread.start()
+
     # getting into loop
     while True:
+        # printing everything in the REQUEST_DONE queue & clearing it
+        if len(REQUEST_DONE) > 0:
+            print("\n---------------- RE-TRIED REQUESTS ----------------")
+            for req in REQUEST_DONE:
+                if len(req) == 3:
+                    print(
+                        f"Request ID : {req[0]} -> {req[1]} -> Account balance : {req[2]}"
+                    )
+                else:
+                    print(f"Request ID : {req[0]} -> {req[1]}")
+
+            REQUEST_DONE.clear()
+
         # taking input from user
-        command = input("Enter the command : ")
+        command = input("\nEnter the command : ")
 
         # splitting the command
         command = command.split(" ")
         cmd = command[0]
         if cmd == "exit":
+            # joining thread
+            RUN_QUEUE_RETRY.set()
+
             break
 
         # checking & waiting if the channel is ready or not (UNAVAILABLE)
@@ -60,10 +216,22 @@ def client():
             pswd = input("Enter the Password : ")
 
             # preparing a new request & sending
-            request = services2.RegReq(bank=bankName, email=emailID, password=pswd)
+            REQUEST_ID += 1
+            request = services2.RegReq(
+                bank=bankName,
+                email=emailID,
+                password=pswd,
+                clientPort=MY_PORT,
+                reqID=REQUEST_ID,
+            )
             try:
                 response = gatewayStub.signUp(request)
-                print(response.message)
+                print(f"Request ID : {REQUEST_ID} -> {response.message}")
+
+                # checking if error occurred then queue it up at the back
+                if response.successAck == 0:
+                    REQUEST_QUEUE.append([request, 0])
+
             except grpc.RpcError as e:
                 if e.code() == grpc.StatusCode.UNAVAILABLE:
                     print("Gateway server unavailable!")
@@ -88,19 +256,27 @@ def client():
                 continue
 
             # preparing a new request & sending
+            REQUEST_ID += 1
             request = services2.TransactReq(
                 bank=bankName,
                 email=emailID,
                 password=pswd,
                 transactionType=transactionType,
                 amount=amount,
+                clientPort=MY_PORT,
+                reqID=REQUEST_ID,
             )
             try:
                 response = gatewayStub.transact(request)
-                print(response.message)
+                print(f"Request ID : {REQUEST_ID} -> {response.message}")
 
-                # printing current balance
-                print(f"Current Balance : {response.balanceLeft}")
+                # checking if error occurred then queue it up at the back
+                if response.successAck == 0:
+                    REQUEST_QUEUE.append([request, 0])
+
+                # checking if failed or success
+                if response.successAck == 1:
+                    print(f"Account balance : {response.balanceLeft}")
 
             except grpc.RpcError as e:
                 if e.code() == grpc.StatusCode.UNAVAILABLE:
@@ -108,6 +284,76 @@ def client():
                 else:
                     print(f"gRPC error: {e}")
                 continue
+
+        elif cmd == "share":
+            # getting all the inputs
+            senderBank = input("Enter the Sender Bank Name : ")
+            senderEmail = input("Enter the Sender Email ID : ")
+            senderPassword = input("Enter the Sender Password : ")
+            receiverBank = input("Enter the Receiver Bank Name : ")
+            receiverEmail = input("Enter the Receiver Email ID : ")
+            amount = int(input("Enter the Amount : "))
+
+            # checking if SAME sender and receiver
+            if senderBank == receiverBank and senderEmail == receiverEmail:
+                print("Sender and Receiver cannot be the same accounts!")
+                continue
+
+            # preparing a new request & sending
+            REQUEST_ID += 1
+            request = services2.ShareReq(
+                senderBank=senderBank,
+                senderEmail=senderEmail,
+                senderPassword=senderPassword,
+                receiverBank=receiverBank,
+                receiverEmail=receiverEmail,
+                amount=amount,
+                clientPort=MY_PORT,
+                reqID=REQUEST_ID,
+            )
+            try:
+                response = gatewayStub.share(request)
+                print(f"Request ID : {REQUEST_ID} -> {response.message}")
+
+                # checking if error occurred then queue it up at the back
+                if response.successAck == 0:
+                    REQUEST_QUEUE.append([request, 0])
+
+                # checking if failed or success
+                if response.successAck == 1:
+                    print(f"Account balance : {response.balanceLeft}")
+
+            except grpc.RpcError as e:
+                if e.code() == grpc.StatusCode.UNAVAILABLE:
+                    print("Gateway server unavailable!")
+                else:
+                    print(f"gRPC error: {e}")
+                continue
+
+        elif cmd == "failover":
+            # getting all the inputs
+            bankName = input("Enter the Bank Name : ")
+
+            # preparing a new request & sending
+            REQUEST_ID += 1
+            request = services2.FailoverReq(
+                bank=bankName,
+                clientPort=MY_PORT,
+                reqID=REQUEST_ID,
+            )
+            try:
+                response = gatewayStub.failover(request)
+                print(f"Request ID : {REQUEST_ID} -> {response.message}")
+
+                # checking if failed then adding to the queue
+                if response.ack == 0:
+                    REQUEST_QUEUE.append([request, 0])
+
+            except grpc.RpcError as e:
+                if e.code() == grpc.StatusCode.UNAVAILABLE:
+                    print("Gateway server unavailable!")
+                else:
+                    print(f"gRPC error: {e}")
 
         else:
             # printing for error
